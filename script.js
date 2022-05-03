916927a0c2262b088e9cf76cbce714ba807d3263
aioconn <76408653+aioconn@users.noreply.github.com>
2022-05-03 10:17:32 -0400

    Added majority of the structure of the website

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ window.onload = function (){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
     const resize = () => {
-        canvas.width = window.innerWidth;
+        canvas.width = window.innerWidth - 20;
         canvas.height = window.innerHeight;
     }
     resize()
+    window.addEventListener('resize', resize)
 }
 
